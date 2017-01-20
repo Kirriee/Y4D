a725a4c1ce1e44c31b29bc6f23405e453c27ceb5
Jessy VD <jessyvandinther@gmail.com>
2017-01-20 16:39:29 +0100

    latest version of home

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,20 @@
 $('.carousel.carousel-slider').carousel({full_width: true});
+
+$('.homeimage').hide().fadeIn(4000)
+
+// load / link jquery in html
+// decide which html element you want to target
+// decide what this element should do
+
+// Begin --> Eind
+// ++++ Begin: opacity 0%
+// $('.homeimage').fadeIn()
+// MAGIC 
+// ====> Eind: opactity 100%
+
+// $('.homeimage').css('opacity', '0')
+
+// fadeOut
+// Begin --> Eind
+// Begin: opacity 100%
+// Eind: opactity 0%
