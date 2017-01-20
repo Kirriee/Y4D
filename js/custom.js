9cce96340287e44825620bbf01f2898f2de35209
Marijke van der Veer <mjvanderveer@gmail.com>
2017-01-20 16:28:39 +0100

    updated menu, added icons to footer

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
 $('.carousel.carousel-slider').carousel({full_width: true});
+
+// var target = $('.dropdown-button')
+// var data = $(target).data('hallo')
+// console.log(data)
+
+
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
